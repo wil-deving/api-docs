9f022f70b0b55eb9390e260d3c58481a80b65b05
wil-deving <wroger.gutierrez@gmail.com>
2023-01-27 22:50:56 -0400

    improving code adding prettier format

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,26 @@
 // pages/_app.js
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
 
-import { extendTheme } from '@chakra-ui/react'
-import { AppProps } from 'next/app'
+import { extendTheme } from "@chakra-ui/react";
+import { AppProps } from "next/app";
 
 // 2. Extend the theme to include custom colors, fonts, etc
 const colors = {
   brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
   },
-}
+};
 
-const theme = extendTheme({ colors })
+const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-import { Button, Flex } from '@chakra-ui/react'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import { Button, Flex } from "@chakra-ui/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -30,7 +30,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -123,5 +123,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
